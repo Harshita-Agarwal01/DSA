5499ae379848f45430c67faa342caa09336f8ced
Harshita Agarwal <harshitaagarwal1411@gmail.com>
2026-09-20 21:28:27 +0530

    Leetcode 503 Next Greater Element II

---
+++ Leetcode/stacks & queues/503_next_greater_element_II.py
@@ -0,0 +1,14 @@
+# TC=O(n)  SC=O(n)
+class Solution:
+    def nextGreaterElements(self, nums: list[int]) -> list[int]:
+        n = len(nums)
+        stack = []
+        ans = [-1] * n
+        for i in range(2 * n - 1, -1, -1):
+            while stack and stack[-1] <= nums[i % n]:
+                stack.pop()
+            if i < n:
+                if stack:
+                    ans[i] = stack[-1]
+            stack.append(nums[i % n])
+        return ans
